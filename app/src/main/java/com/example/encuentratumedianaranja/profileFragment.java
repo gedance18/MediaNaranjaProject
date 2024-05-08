73abb382a7e9662ba1f02c176d8d4eaefdf7c770
gedance18 <gedance18yt@gmail.com>
2024-05-08 19:52:07 +0200

    barrita de abajo lista

--- app/src/main/java/com/example/encuentratumedianaranja/profileFragment.java
+++ app/src/main/java/com/example/encuentratumedianaranja/profileFragment.java
@@ -5,40 +5,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class profileFragment extends Fragment {
 
-    private static final String ARG_PROFILE_IMAGE_URL = "profile_image_url";
-    private static final String ARG_NAME = "name";
-    private static final String ARG_AGE = "age";
-
-    private String profileImageUrl;
-    private String name;
-    private String age;
-
-    public static profileFragment newInstance(String profileImageUrl, String name, String age) {
-        profileFragment fragment = new profileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PROFILE_IMAGE_URL, profileImageUrl);
-        args.putString(ARG_NAME, name);
-        args.putString(ARG_AGE, age);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            profileImageUrl = getArguments().getString(ARG_PROFILE_IMAGE_URL);
-            name = getArguments().getString(ARG_NAME);
-            age = getArguments().getString(ARG_AGE);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        if (activity != null) {
+            activity.setSupportActionBar(view.findViewById(R.id.toolbar));
+
+            NavHostFragment navHostFragment = (NavHostFragment) activity.getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+            if (navHostFragment != null) {
+                NavController navController = navHostFragment.getNavController();
+                BottomNavigationView bottomNavView = view.findViewById(R.id.bottom_nav_view);
+                NavigationUI.setupWithNavController(bottomNavView, navController);
+            }
         }
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return view;
     }
 }
